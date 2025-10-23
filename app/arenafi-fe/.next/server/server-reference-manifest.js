34be1f8ba462b3de0d8b5e67dff0b0f752f3f590
0xfabb <vanshdev2305@gmail.com>
2025-10-23 14:22:00 +0530

    added mvp features

--- app/arenafi-fe/.next/server/server-reference-manifest.js
+++ app/arenafi-fe/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qaZlGuMsR1804CjVf+Mzmcq50H6XVib+ibk74YsT6l8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vopDe+UBBIcPh6clVMh8xx+BuY9aOBsYk/7Y1b3ntlA=\"\n}"
